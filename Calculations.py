10c8538d17dfbf8acdefe94009615da3c01ffe15
Jonathan <yk1993@gmail.com>
2020-04-06 18:18:02 +0300

    added Calculations.py with the methods: find_csv_filename - create array of all the shares avg_and_stdev_per_day_per_share - creating new file that holds the average and the standard deviation for each Share in the ShareData directory

--- Calculations.py
+++ Calculations.py
@@ -1,4 +1,7 @@
+# Calculations.py
+
 import pandas as pd
+import numpy as np
 from statistics import stdev
 from os import listdir
 
@@ -13,6 +16,7 @@ def avg_and_stdev_per_day_per_share():
 
     # set the correct path according to the project location    V
     all_files = find_csv_filename("D:\Coding\Python\PyCharm\PyCharm\ShareData")
+
     # print(all_files)  debugging - delete
 
     avgArr = []
@@ -21,15 +25,75 @@ def avg_and_stdev_per_day_per_share():
     for f in all_files:
         # print(f)  debugging - delete
         market = pd.read_csv('ShareData/' + f)
-        rowCount = market.iloc[2:, 2].astype(float)
-        closingSum = rowCount.sum()
+        closingRowData = market.iloc[2:, 2].astype(float)
+        closingSum = closingRowData.sum()
         openingSum = market.iloc[2:, 4].astype(float).sum()
-        avg = (closingSum + openingSum) / (rowCount.size * 2) # Calculating the whole average
+        avg = (closingSum + openingSum) / (closingRowData.size * 2) # Calculating the whole average
         avgArr.append(avg)
-        stdevArr.append(stdev(rowCount))
+        stdevArr.append(stdev(closingRowData))
 
     finished = {'Name of the Share': all_files, 'Average': avgArr, 'Standard Deviation': stdevArr}
     calc = pd.DataFrame(finished)
     calc.to_csv('avg_and_stdev_per_day_per_share.csv')
 
-    print(calc)
+   # print(calc)  debugging - delete
+
+
+def avg_and_beta_per_day_per_share():
+    # set the correct path according to the project location    V
+    all_files = find_csv_filename("D:\Coding\Python\PyCharm\PyCharm\ShareData")
+
+    # print(all_files)  debugging - delete
+
+    avgArr = []
+    tempBetaArr = []
+    betaArr = []
+
+    for f in all_files:
+        # print(f)  debugging - delete
+        market = pd.read_csv('ShareData/' + f)
+        closingRowData = market.iloc[2:, 2].astype(float)
+        closingSum = closingRowData.sum()
+        openingSum = market.iloc[2:, 4].astype(float).sum()
+        avg = (closingSum + openingSum) / (closingRowData.size * 2)  # Calculating the whole average
+        avgArr.append(avg)
+
+        for i in range(2, closingRowData.size-1):
+            betaRow = (closingRowData[i] / closingRowData[i+1]) -1
+            tempBetaArr.append(betaRow)
+
+        betaArr.append(np.average(tempBetaArr))
+
+    finished = {'Name of the Share': all_files, 'Average': avgArr, 'Beta': betaArr}
+    calc = pd.DataFrame(finished)
+    calc.to_csv('avg_and_beta_per_day_per_share.csv')
+
+# print(calc)  debugging - delete
+
+
+
+
+def returns_per_year_per_share():
+    all_files = find_csv_filename("D:\Coding\Python\PyCharm\PyCharm\ShareData")
+
+    yearArr = [2013, 2014, 2015, 2016, 2017, 2018]
+    tempRetArr = []
+    retaArr = []
+
+    for f in all_files:
+        # print(f)  debugging - delete
+        market = pd.read_csv('ShareData/' + f)
+
+        closingRowData = market.iloc[2:, 2].astype(float)
+
+        for i in range(2, closingRowData.size - 1):
+            betaRow = (closingRowData[i] / closingRowData[i + 1]) - 1
+            tempRetArr.append(betaRow)
+
+        retaArr.append(np.average(tempRetArr))
+
+
+
+        finished = {'Name of the Share': all_files, 'Return': retaArr, 'Year': yearArr}
+        calc = pd.DataFrame(finished)
+        calc.to_csv('avg_and_beta_per_day_per_share.csv')
--- main.py
+++ main.py
@@ -1,34 +1,42 @@
+# main.py
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-from sklearn.cluster import KMeans
 import Calculations
+from sklearn.cluster import KMeans, SpectralClustering
 
-Market = pd.read_csv('ShareData/afi.csv')
-# print(Market)
+# Calculations.avg_and_stdev_per_day_per_share() #Creating the calculated file for Seif A
+# Calculations.avg_and_beta_per_day_per_share() #Creating the calculated file for Seif B
 
-X = Market.iloc[2:,[2,3]].values
+Market = pd.read_csv('avg_and_stdev_per_day_per_share.csv')
 
-#X = Calculations.avg_per_day_per_share('afi.csv')
+# Using the K-Means algorithm with the calculated data:
+X = Market.iloc[2:, [2, 3]].values
 
-Calculations.avg_and_stdev_per_day_per_share()
+
+# kmeans = KMeans(n_clusters=5,)
+# y_kmeans = kmeans.fit_predict(X)
 
 
-#avg and teken for X
 
-kmeans = KMeans(n_clusters=5)
-y_kmeans = kmeans.fit_predict(X)
+model = SpectralClustering(n_clusters=2, affinity='nearest_neighbors',
+                           assign_labels='kmeans')
+y_kmeans = model.fit_predict(X)
 
+
+# Creating a chart of the calculated K-means algorithm:  REGARDING Seif A & B
 plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], label = 'cluster1')
 plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], label = 'cluster2')
 plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], label = 'cluster3')
 plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], label = 'cluster4')
 plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], label = 'cluster5')
-
-plt.title('Clusters')
-plt.xlabel('AVG????')
-plt.ylabel('TEKEN???')
+plt.title('Average and Standard Deviation Table')
+plt.xlabel('Average')
+plt.ylabel('Standard Deviation')
 plt.legend()
 
-#plt.show()
+plt.show()
+
+
+
